a314980d586145d0046fbb8972482c59a2f992e7
UltimatChamp <UltimatChamp@proton.me>
2025-01-27 13:22:14 +0530

    feat: badges based on creative menu tabs

--- src/main/java/dev/ultimatchamp/enhancedtooltips/component/HeaderTooltipComponent.java
+++ src/main/java/dev/ultimatchamp/enhancedtooltips/component/HeaderTooltipComponent.java
@@ -11,17 +11,6 @@ import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import org.joml.Matrix4f;
 
-//? if >1.21.1 {
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.ConsumableComponent;
-import net.minecraft.component.type.FoodComponent;
-//?} else if >1.20.4 {
-/*import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.FoodComponent;
-*///?} else {
-/*import net.minecraft.item.FoodComponent;
-*///?}
-
 public class HeaderTooltipComponent implements TooltipComponent {
     private static final int TEXTURE_SIZE = 24;
     private static final int ITEM_MODEL_SIZE = 16;
@@ -46,35 +35,20 @@ public class HeaderTooltipComponent implements TooltipComponent {
         int badgeWidth = 0;
 
         if (EnhancedTooltipsConfig.load().itemBadges) {
-            if (this.stack.getItem() instanceof ArmorItem || this.stack.getItem() instanceof
-                    //? if >1.20.4 {
-                    AnimalArmorItem) {
-                    //?} else {
-                    /*HorseArmorItem) {
-                    *///?}
-                badgeWidth = textRenderer.getWidth(Text.translatable("attribute.name.armor")) + SPACING * 3;
-            } else if (this.stack.isDamageable()) {
-                badgeWidth = textRenderer.getWidth(Text.translatable("itemGroup.tools")) + SPACING * 3;
-            } else if (this.stack.getItem() instanceof EntityBucketItem) {
-                badgeWidth = textRenderer.getWidth(Text.translatable("item.minecraft.bucket")) + SPACING * 3;
-            } else if (this.stack.getItem() instanceof SpawnEggItem) {
-                badgeWidth = textRenderer.getWidth(Text.translatable("itemGroup.spawnEggs")) + SPACING * 3;
-            } else if (this.stack.getItem() instanceof BlockItem) {
-                badgeWidth = textRenderer.getWidth(Text.translatable("soundCategory.block")) + SPACING * 3;
-            } else {
-                //? if >1.21.1 {
-                ConsumableComponent consumableComponent = stack.get(DataComponentTypes.CONSUMABLE);
-                FoodComponent foodComponent = stack.getItem().getComponents().get(DataComponentTypes.FOOD);
-                if ((foodComponent != null && consumableComponent != null) || stack.getItem() instanceof PotionItem) {
-                //?} else if >1.20.4 {
-                /*FoodComponent foodComponent = stack.getItem().getComponents().get(DataComponentTypes.FOOD);
-                if (foodComponent != null) {
-                *///?} else {
-                /*FoodComponent foodComponent = stack.getItem().getFoodComponent();
-                if (foodComponent != null) {
-                *///?}
-                    badgeWidth = textRenderer.getWidth(Text.translatable("itemGroup.foodAndDrink")) + SPACING * 3;
+            for (var tab : ItemGroups.getGroups()) {
+                if (tab.equals(ItemGroups.getSearchGroup())) continue;
+
+                var identified = false;
+
+                for (var displayStack : tab.getDisplayStacks()) {
+                    if (stack.isOf(displayStack.getItem())) {
+                        badgeWidth = textRenderer.getWidth(tab.getDisplayName()) + SPACING * 3 - 2;
+                        identified = true;
+                        break;
+                    }
                 }
+
+                if (identified) break;
             }
         }
 
@@ -106,44 +80,51 @@ public class HeaderTooltipComponent implements TooltipComponent {
         int startDrawX = x + (TEXTURE_SIZE - ITEM_MODEL_SIZE) / 2;
         int startDrawY = y + (TEXTURE_SIZE - ITEM_MODEL_SIZE) / 2;
 
-        drawFrame(context, x, y + 1, TEXTURE_SIZE, TEXTURE_SIZE, 400, 0xffd6d6d6, 0xffd6d6d6);
         context.drawItem(this.stack, startDrawX, startDrawY);
 
         if (!EnhancedTooltipsConfig.load().itemBadges) return;
 
-        if (this.stack.getItem() instanceof ArmorItem || this.stack.getItem() instanceof
-                //? if >1.20.4 {
-                AnimalArmorItem) {
-                //?} else {
-                /*HorseArmorItem) {
-                *///?}
-            drawBadge(textRenderer, Text.translatable("attribute.name.armor"), x, y, context, 0xff00ffff, 0xffc8ffff, 0xff6ac7be);
-        } else if (this.stack.isDamageable()) {
-            drawBadge(textRenderer, Text.translatable("itemGroup.tools"), x, y, context, 0xff57b88c, 0xff6acda4, 0xff4e866f);
-        } else if (this.stack.getItem() instanceof EntityBucketItem) {
-            drawBadge(textRenderer, Text.translatable("item.minecraft.bucket"), x, y, context, 0xff14564b, 0xff3d8075, 0xff367166);
-        } else if (this.stack.getItem() instanceof SpawnEggItem) {
-            drawBadge(textRenderer, Text.translatable("itemGroup.spawnEggs"), x, y, context, 0xff70589b, 0xffa994cf, 0xffa994ff);
-        } else if (this.stack.getItem() instanceof BlockItem) {
-            drawBadge(textRenderer, Text.translatable("soundCategory.block"), x, y, context, 0xff396425, 0xff496c67, 0xff38502b);
-        } else {
-            //? if >1.21.1 {
-            ConsumableComponent consumableComponent = stack.get(DataComponentTypes.CONSUMABLE);
-            FoodComponent foodComponent = stack.getItem().getComponents().get(DataComponentTypes.FOOD);
-            if ((foodComponent != null && consumableComponent != null) || stack.getItem() instanceof PotionItem) {
-            //?} else if >1.20.4 {
-            /*FoodComponent foodComponent = stack.getItem().getComponents().get(DataComponentTypes.FOOD);
-            if (foodComponent != null) {
-            *///?} else {
-            /*FoodComponent foodComponent = stack.getItem().getFoodComponent();
-            if (foodComponent != null) {
-            *///?}
-                drawBadge(textRenderer, Text.translatable("itemGroup.foodAndDrink"), x, y, context, 0xff61b748, 0xff7ad85b, 0xff8ab85b);
+        for (var tab : ItemGroups.getGroups()) {
+            if (tab.equals(ItemGroups.getSearchGroup())) continue;
+
+            var identified = false;
+
+            for (var displayStack : tab.getDisplayStacks()) {
+                if (stack.isOf(displayStack.getItem())) {
+                    if (tab.contains(Items.NETHERITE_PICKAXE.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xff9b59b6, 0xff8e44ad); // Tools & Utilities
+                    } else if (tab.contains(Items.NETHERITE_SWORD.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xfff94144, 0xffd72638); // Combat
+                    } else if (tab.contains(Items.REPEATER.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xffff6b6b, 0xffe63939); // Redstone Blocks
+                    } else if (tab.contains(Items.OAK_SIGN.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xff2a9d8f, 0xff207567); // Functional Blocks
+                    } else if (tab.contains(Items.COOKED_BEEF.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xff61b748, 0xff7ad85b); // Food and Drinks
+                    } else if (tab.contains(Items.GOLD_INGOT.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xffff6347, 0xffffa07a); // Ingredients
+                    } else if (tab.contains(Items.ALLAY_SPAWN_EGG.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xffa29bfe, 0xff817dc4); // Spawn Eggs
+                    } else if (tab.contains(Items.COMMAND_BLOCK.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xff9c89b8, 0xff7a6395); // Operator Utilities
+                    } else if (tab.contains(Items.GRASS_BLOCK.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xff66bb6a, 0xffa5d6a7); // Natural Blocks
+                    } else if (tab.contains(Items.BLUE_WOOL.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xff42a5f5, 0xff90caf9); // Colored Blocks
+                    } else if (tab.contains(Items.IRON_BARS.getDefaultStack())) {
+                        drawBadge(textRenderer, tab.getDisplayName(), x, y, context, 0xfff2c94c, 0xffe0a800); // Building Blocks
+                    }
+
+                    identified = true;
+                    break;
+                }
             }
+
+            if (identified) break;
         }
     }
 
-    private void drawBadge(TextRenderer textRenderer, Text text, int x, int y, DrawContext context, int fillColor, int startColor, int endColor) {
+    private void drawBadge(TextRenderer textRenderer, Text text, int x, int y, DrawContext context, int fillColor, int borderColor) {
         int textWidth = textRenderer.getWidth(text);
         int textHeight = textRenderer.fontHeight;
 
@@ -151,7 +132,7 @@ public class HeaderTooltipComponent implements TooltipComponent {
         int textY = y - textRenderer.fontHeight + SPACING * 2 + 2;
 
         context.fill(
-                textX - SPACING,
+                textX - SPACING - 2,
                 textY - SPACING / 2,
                 textX + textWidth + SPACING,
                 textY + textHeight,
@@ -161,7 +142,7 @@ public class HeaderTooltipComponent implements TooltipComponent {
         context.drawText(
                 textRenderer,
                 text,
-                textX,
+                textX - 2,
                 textY,
                 0xffffffff,
                 true
@@ -169,25 +150,24 @@ public class HeaderTooltipComponent implements TooltipComponent {
 
         drawFrame(
                 context,
-                textX - SPACING,
+                textX - SPACING - 2,
                 textY - SPACING / 2,
                 textWidth + SPACING * 2,
                 textHeight + SPACING,
                 400,
-                startColor,
-                endColor
+                borderColor
         );
     }
 
-    private static void drawFrame(DrawContext context, int x, int y, int width, int height, int z, int startColor, int endColor) {
-        renderVerticalLine(context, x, y, height - 2, z, startColor, endColor);
-        renderVerticalLine(context, x + width - 1, y, height - 2, z, startColor, endColor);
-        renderHorizontalLine(context, x + 1, y - 1, width - 2, z, startColor);
-        renderHorizontalLine(context, x + 1, y - 1 + height - 1, width - 2, z, endColor);
+    private static void drawFrame(DrawContext context, int x, int y, int width, int height, int z, int color) {
+        renderVerticalLine(context, x, y, height - 2, z, color);
+        renderVerticalLine(context, x + width - 1, y, height - 2, z, color);
+        renderHorizontalLine(context, x + 1, y - 1, width - 2, z, color);
+        renderHorizontalLine(context, x + 1, y - 1 + height - 1, width - 2, z, color);
     }
 
-    private static void renderVerticalLine(DrawContext context, int x, int y, int height, int z, int startColor, int endColor) {
-        context.fillGradient(x, y, x + 1, y + height, z, startColor, endColor);
+    private static void renderVerticalLine(DrawContext context, int x, int y, int height, int z, int color) {
+        context.fill(x, y, x + 1, y + height, z, color);
     }
 
     private static void renderHorizontalLine(DrawContext context, int x, int y, int width, int z, int color) {
