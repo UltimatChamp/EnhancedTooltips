479dfc28cd9acf0e47d9293c855c53495a33852e
EnotikSergo <78279790+EnotikSergo@users.noreply.github.com>
2025-09-06 23:09:57 +0300

    Add files via upload

--- src/main/java/dev/ultimatchamp/enhancedtooltips/tooltip/TooltipHelper.java
+++ src/main/java/dev/ultimatchamp/enhancedtooltips/tooltip/TooltipHelper.java
@@ -9,6 +9,10 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
 import net.minecraft.util.Colors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TooltipHelper {
     public static Text getRarityName(ItemStack stack) {
         String key = EnhancedTooltips.MOD_ID + ".rarity." + stack.getRarity().name().toLowerCase();
@@ -25,23 +29,49 @@ public class TooltipHelper {
         *///?}
     }
 
-    public static int getItemBorderColor(ItemStack stack) {
-        Integer color = null;
+    public static List<Integer> getItemBorderColor(ItemStack stack) {
+        List<Integer> color = new ArrayList<>(Arrays.asList(null, null));;
 
         if (EnhancedTooltipsConfig.load().border.borderColor == EnhancedTooltipsConfig.BorderColorMode.ITEM_NAME) {
             Text displayName = getDisplayName(stack);
+            List<Text> outerSiblings = displayName.getSiblings();
 
-            color = TranslationStringColorParser.getColorFromTranslation(displayName);
-
-            TextColor textColor = displayName.getStyle().getColor();
-            if (color == -1 && textColor != null) color = displayName.getStyle().getColor().getRgb();
+            if (outerSiblings != null && !outerSiblings.isEmpty()) {
+                for (Text outerSibling : outerSiblings) {
+                    List<Text> innerSiblings = outerSibling.getSiblings();
+                    if (innerSiblings != null && !innerSiblings.isEmpty()) {
+                        for (Text innerSibling : innerSiblings) {
+                            TextColor siblingColor = innerSibling.getStyle().getColor();
+                            if (siblingColor != null) {
+                                if (color.get(0) == null) {
+                                    color.set(0, siblingColor.getRgb());
+                                }
+                                color.set(1, siblingColor.getRgb());
+                            }
+                        }
+                    }
+                    else {
+                        TextColor siblingColor = outerSibling.getStyle().getColor();
+                        if (siblingColor != null) {
+                            color.set(0, siblingColor.getRgb());
+                            color.set(1, siblingColor.getRgb());
+                            break;
+                        }
+                    }
+                }
+            }
+            if (color.get(0) == null || color.get(0) == -1) {
+                Integer clr = stack.getRarity().getFormatting().getColorValue();
+                color.set(0, clr);
+                color.set(1, clr);
+            }
         }
-
-        if (color == null || color == -1 || color == 0xFFFFFF) {
-            color = stack.getRarity().getFormatting().getColorValue();
-            if (color == null || color == 0xFFFFFF) color = -1;
+        else{
+            if (color.get(0) == null || color.get(0) == -1 || color.get(0) == 0xFFFFFF) {
+                color.set(0, stack.getRarity().getFormatting().getColorValue());
+                if (color.get(0) == null || color.get(0) == 0xFFFFFF) color.set(0, -1);
+            }
         }
-
         return color;
     }
 }
